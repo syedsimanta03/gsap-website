cd5be485165893769f1fc721663ed4c1c4070c71
syedsimanta03 <syed.simanta06@gmail.com>
2022-08-07 11:02:56 +0600

    parallaxsection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+import logo from './assets/logo.svg'
+import styled from 'styled-components'
+import ParallaxHeroSection from './components/ParallaxHeroSection'
+
+const StyledHeroSection = styled.section`
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  img {
+    height: 25vmin;
+    pointer-events: none;
+  }
+`
+
+const StyledTitle = styled.h1`
+  color: black;
+  font-size: 2rem;
+  font-wight: 400;
+  letter-spacing: 4px;
+  text-align: center;
+  text-transform: uppercase;
+  padding-top: 4rem;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
-  );
+    <StyledHeroSection>
+      <img src={logo} alt='logo' />
+      <StyledTitle>Back to smooth and firm skin</StyledTitle>
+      <ParallaxHeroSection/>
+    </StyledHeroSection>
+  )
 }
 
 export default App;
